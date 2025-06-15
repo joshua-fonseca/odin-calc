9e007bce1cac7af2ecccd0003401873af44cae45
joshua-fonseca <joshuacfonseca@gmail.com>
2025-06-15 15:15:28 -0400

    complete steps 1-3 of calculator project - basic functions - three variables for each part of the operation - create function operate that calls the basic functions - included inspiration layout (Apple calculator app)

--- script.js
+++ script.js
@@ -1,1 +1,34 @@
 console.log("1");
+
+let operand1 = 3;
+let operand2 = 5;
+let operator = '+';
+
+const add = (a, b) => {
+  return a + b;
+}
+
+const subtract = (a, b) => {
+  return a - b;
+}
+
+const multiply = (a, b) => {
+  return a * b;
+}
+
+const divide = (a, b) => {
+  return a / b;
+}
+
+const operate = (operator, a, b) => {
+  switch(operator) {
+    case '+':
+      return add(a, b);
+    case '-':
+      return subtract(a, b);
+    case '*':
+      return multiply(a, b);
+    case '/':
+      return divide(a, b);
+  }
+}
